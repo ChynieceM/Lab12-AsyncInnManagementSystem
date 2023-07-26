7fb14baaaa2100221ee583fb81416737cacfb6e1
Chyniece Matthews <cfmatthews2@gmail.com>
2023-07-26 13:48:03 -0500

    Add project files.

---
+++ Lab12-AsyncInnManagementSystem/Program.cs
@@ -0,0 +1,15 @@
+namespace Lab12_AsyncInnManagementSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
